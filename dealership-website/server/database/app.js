c5886752effabee788417323e00c0dfc88c8ce79
Ayush Pandey <inbox.ayushpandey@gmail.com>
2024-02-24 23:16:35 +0530

    added CarMake and CarModel model and created get_cars view

--- dealership-website/server/database/app.js
+++ dealership-website/server/database/app.js
@@ -8,6 +8,7 @@ const port = 3030;
 app.use(cors())
 app.use(require('body-parser').urlencoded({ extended: false }));
 
+
 const reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
 const dealerships_data = JSON.parse(fs.readFileSync("dealerships.json", 'utf8'));
 
@@ -15,7 +16,6 @@ mongoose.connect("mongodb://mongo_db:27017/", { 'dbName': 'dealershipsDB' });
 
 
 const Reviews = require('./review');
-
 const Dealerships = require('./dealership');
 
 try {
